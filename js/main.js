66426654b7e7c91266fbaf0a7140e6ca20d1213d
Jose Gonzalez <joseraul88@gmail.com>
2022-08-28 16:56:03 -0500

    Added 3 els to menu  and firewood section

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const header = document.querySelector("header");
+const heroSection = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      header.classList.add("header-background");
+    } else if (ent.isIntersecting) {
+      header.classList.remove("header-background");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0.85,
+  }
+);
+
+obs.observe(heroSection);
