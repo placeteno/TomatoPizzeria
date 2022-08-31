67ab089dddca1eb34247423e80ff4701ce634ed3
Jose Gonzalez <joseraul88@gmail.com>
2022-08-31 10:57:48 -0500

    Project completed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 const header = document.querySelector("header");
+const navMenu = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
+const openNavBtn = document.querySelector('ion-icon[name="menu-outline"]');
+const closeNavBtn = document.querySelector('ion-icon[name="close-outline"]');
 const heroSection = document.querySelector(".hero-section");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("nav-open");
+    openNavBtn.style.display = "block";
+  });
+});
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
     if (!ent.isIntersecting) {
       header.classList.add("header-background");
     } else if (ent.isIntersecting) {
@@ -17,5 +27,21 @@ const obs = new IntersectionObserver(
     threshold: 0.95,
   }
 );
-
 obs.observe(heroSection);
+
+openNavBtn.addEventListener("click", () => {
+  navMenu.classList.add("nav-open");
+  openNavBtn.style.display = "none";
+});
+
+closeNavBtn.addEventListener("click", () => {
+  navMenu.classList.remove("nav-open");
+  openNavBtn.style.display = "block";
+});
+
+document.querySelector("main").addEventListener("click", () => {
+  if (navMenu.classList.contains("nav-open")) {
+    navMenu.classList.remove("nav-open");
+    openNavBtn.style.display = "block";
+  }
+});
